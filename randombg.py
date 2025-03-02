110abacb7c26a771be3281dbdd36de6c70027c16
Jacob Wesson <jacobwesson434@gmail.com>
2025-03-02 11:26:57 -0500

    cleaned up a bit

--- randombg.py
+++ randombg.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
+from collections.abc import Callable
 import os
-import re
 import random
-import fileinput
 
 # Configure these:
 IMAGES_DIRECTORY_PATH: str = "/home/ethan/backgrounds"
@@ -12,17 +11,17 @@ VALID_FILE_FORMATS = ["png", "jpeg", "gif", "webp", "jpg"]
 
 # takes in a list of file names
 # returns one with the valid file formats
-def filterImages(files: [str]) -> [str]:
+def filterImages(files: list[str]) -> list[str]:
     breakAtDot = lambda file: file.split(".")
-    extentionSeperatedFiles: [[str]] = map(breakAtDot, files)
+    extentionSeperatedFiles: map[Callable] = map(breakAtDot, files)
     
     getLastExtention = lambda extentionSeperatedFile: extentionSeperatedFile[-1]
-    fileExtentions: [str] = map(getLastExtention, extentionSeperatedFiles)
+    fileExtentions: map[str] = map(getLastExtention, extentionSeperatedFiles)
 
     isImg = lambda fileExtention: fileExtention in VALID_FILE_FORMATS
-    areImages: [bool] = list(map(isImg, fileExtentions))
+    areImages: list[bool] = list(map(isImg, fileExtentions))
     
-    validFiles: [str] = []
+    validFiles: list[str] = []
     for i in range(len(files)):
         if areImages[i]:
             validFiles.append(files[i])
@@ -33,19 +32,19 @@ def filterImages(files: [str]) -> [str]:
 # returns a str the absolute path of a random 
 # image in the directory 
 def getRandomBackground(directory: str) -> str:
-    dirListing: [str] = os.listdir(directory)
-    validBackgrounds: [str] = filterImages(dirListing)
-    chosenPicture: int = random.choice(validBackgrounds)
-    absolutePathOfChosenPicture = IMAGES_DIRECTORY_PATH + "/" + chosenPicture
+    dirListing: list[str] = os.listdir(directory)
+    validBackgrounds: list[str] = filterImages(dirListing)
+    chosenPicture: str = random.choice(validBackgrounds)
+    absolutePathOfChosenPicture: str = IMAGES_DIRECTORY_PATH + "/" + chosenPicture
     return absolutePathOfChosenPicture
 
 def setConfig(backgroundImagePath: str):
     preload: str = "preload = " + backgroundImagePath
     wallpaper: str = "wallpaper = monitor, " + backgroundImagePath
     newConfig: str = "\n".join([preload, wallpaper])
-    
-    with open(HYPRPAPER_PATH, "w") as hyperpaperConfig:
-        hyperpaperConfig.write(newConfig)
+    print(newConfig)
+    #with open(HYPRPAPER_PATH, "w") as hyperpaperConfig:
+    #    hyperpaperConfig.write(newConfig)
 
 setConfig(getRandomBackground(IMAGES_DIRECTORY_PATH))
 
